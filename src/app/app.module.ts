c62710f003ad0666ef809ac06d3ebf2a52f00502
shashidhar-gr <shashidhargr26@gmail.com>
2020-01-27 21:40:21 +0530

    Fixed error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CustomerAuthComponent } from './customer-login/customer-auth/customer-auth.component';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    CustomerAuthComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
